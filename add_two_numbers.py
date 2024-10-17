252389f6dc535798156c878f22b16ed3fc2d665f
JanCinceraJergym <jan.cincera@jergym.cz>
2024-10-17 14:46:07 +0200

    add two numbers: final

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,28 +1,33 @@
 from lc import *
 
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        def list_to_int(l: ListNode) -> int:
-            res = 0
-            i = 0
-            cl = l
-            while cl is not None:
-                res += cl.val * 10**i
-                i+=1
-                cl = cl.next
-            return res
+    @staticmethod
+    def _list_to_int(l: ListNode) -> int:
+        res = 0
+        i = 0
+        cl = l
+        while cl is not None:
+            res += cl.val * 10**i
+            i+=1
+            cl = cl.next
+        return res
 
-        rem = list_to_int(l1) + list_to_int(l2)
-        print(rem)
+    @staticmethod
+    def _int_to_list(n: int) -> ListNode:
+        rem = n
         node = ListNode()
         root = node
         while rem > 0:
             node.val = rem % 10
-            if rem > 10:
-                node.next = ListNode()
-                node = node.next
-            else:
+            if rem < 10:
                 node.next = None
+            else:
+                node.next = ListNode()
+            node = node.next
             rem //= 10
         return root
+
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        return self._int_to_list(self._list_to_int(l1) + self._list_to_int(l2))
+        
 
--- test_add_two_numbers.py
+++ test_add_two_numbers.py
@@ -2,23 +2,9 @@ from add_two_numbers import *
 
 sol = Solution()
 
-def gen_linked_list(n: int) -> Optional[ListNode]:
-    rem = n
-    node = ListNode()
-    root = node
-    while rem > 0:
-        node.val = rem % 10
-        if rem > 10:
-            node.next = ListNode()
-            node = node.next
-        else:
-            node.next = None
-        rem //= 10
-    return root
-
 def test1():
-    assert gen_linked_list(14) == gen_linked_list(14)
+    assert sol._int_to_list(14) == sol._int_to_list(14)
 def test2():
-    assert sol.addTwoNumbers(gen_linked_list(342), gen_linked_list(465)) == gen_linked_list(807)
+    assert sol.addTwoNumbers(sol._int_to_list(342), sol._int_to_list(465)) == sol._int_to_list(807)
 def test3():
-    assert sol.addTwoNumbers(gen_linked_list(9999999), gen_linked_list(9999)) == gen_linked_list(10009998)
+    assert sol.addTwoNumbers(sol._int_to_list(9999999), sol._int_to_list(9999)) == sol._int_to_list(10009998)
